20c5273c1560d069f6a471e47e73d7a336e092f8
awwa500@gmail.com <awwa500@gmail.com>
2015-11-01 16:29:17 +0900

    12

--- server/publications.js
+++ server/publications.js
@@ -1,9 +1,17 @@
-Meteor.publish('posts', function(/*author*/){
-  return Posts.find(
+Meteor.publish('posts', function(options/*author*/){
+  // check(options, {
+  //   sort: Object,
+  //   limit: Number
+  // });
+  return Posts.find({}, options
     /*{}, {fields: {title: false}}*/
     /*{'author':'Tom Coleman'}*/
     /*{flagged: false, author: auther}*/
   );
+});
+Meteor.publish('singlePost', function(id){
+  //check(id, String);
+  return Posts.find(id);
 });
 Meteor.publish('comments', function(postId) {
   //check(postId, String);
